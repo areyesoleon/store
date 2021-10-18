fbf7b0e85a7844c1c389375e3dddb10335419783
Oscar Reyes <oreyes@grupotecun.com>
2021-10-18 15:23:27 -0600

    :construction: added form of productos

--- src/app/product/new-edit/new-edit.component.ts
+++ src/app/product/new-edit/new-edit.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormComponent } from 'src/app/core/tools/form.component';
 
 @Component({
   selector: 'app-new-edit',
   templateUrl: './new-edit.component.html',
   styleUrls: ['./new-edit.component.scss']
 })
-export class NewEditComponent implements OnInit {
+export class NewEditComponent extends FormComponent implements OnInit {
 
-  constructor() { }
+  constructor(protected builder: FormBuilder) {
+    super();
+    this.toInitForm();
+  }
+
+  private toInitForm() {
+    this._form = this.builder.group({
+      empleado_id: null,
+      fecha_ingreso: new Date(),
+      flg_activo: true,
+      entidad: this.builder.group({
+        entidad_id: null,
+        nombre: null,
+        apellido: null,
+        dpi: null,
+        nit: null,
+        telefono: null,
+        direccion: null,
+        fecha_nacimiento: new Date(),
+        correo: null
+      })
+    });
+  }
 
   ngOnInit(): void {
+
   }
+
+  toSave() {
+    console.log(this._form.value);
+  }
+
+  toClear() {
+    this.toInitForm();
+  }
+
 
 }
