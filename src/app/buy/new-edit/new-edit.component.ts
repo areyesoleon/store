1e0dfaaeb9d34abe624274e97807ceacf6d27f6d
Oscar Reyes <oreyes@grupotecun.com>
2021-10-19 14:34:35 -0600

    :construction: form of buy added

--- src/app/buy/new-edit/new-edit.component.ts
+++ src/app/buy/new-edit/new-edit.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormComponent } from 'src/app/core/tools/form.component';
 
 @Component({
   selector: 'app-new-edit',
   templateUrl: './new-edit.component.html',
   styleUrls: ['./new-edit.component.scss']
 })
-export class NewEditComponent implements OnInit {
+export class NewEditComponent extends FormComponent implements OnInit {
+  constructor(protected builder: FormBuilder) {
+    super();
+    this.toInitForm();
+  }
 
-  constructor() { }
+  private toInitForm() {
+    this._form = this.builder.group({
+      id: null,
+      proveedor_id: null,
+      sucursal_id: null,
+      estado_compra_id: null,
+      documento: null,
+      fecha: new Date(),
+      detalles: []
+    });
+  }
 
   ngOnInit(): void {
+
+  }
+
+  toSave() {
+    console.log(this._form.value);
+  }
+
+  toClear() {
+    this.toInitForm();
   }
 
 }
